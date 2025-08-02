fe8e1c84a147ce19529a657f583daa7f778f91dc
farhuu0 <mstlimak672@gmail.com>
2025-08-02 14:03:36 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,7 +72,7 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == ""){
+        if(event.reaction == "😠"){
   if(event.userID == "100080736881604","61560912984498"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
@@ -82,7 +82,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction ==  "😠"){
+        if(event.reaction ==  "👍"){
   if(event.senderID == api.getCurrentUserID()){if(event.userID == "100080736881604",""){
     message.unsend(event.messageID)
 }else{
