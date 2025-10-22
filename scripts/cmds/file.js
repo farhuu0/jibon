8ec45f93e28124f208e3c65dd6adf8960484d7d6
FARHAN AHMED <mstlimak672@gmail.com>
2025-10-22 17:32:40 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100080736881604,61578270134835"];
+		const permission = ["100080736881604","61578270134835"];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage("খনিকের পোলা ফাইল তোমার পু৳কি দিয় ভরে দিব 😾.", event.threadID, event.messageID);
 		}
